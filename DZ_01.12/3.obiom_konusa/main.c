cb626cac8fcaab3a96a3898edae90b2fc580a9a0
pirozhnikolga <pirozhenka@yandex.ru>
2014-12-01 15:21:07 +0300

    добавила площадь конуса

--- DZ_01.12/3.obiom_konusa/main.c
+++ DZ_01.12/3.obiom_konusa/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
     printf("Enter radius and height: ");
-    double a, b;
+    double a, b, L;
     scanf("%lf%lf", &a, &b);
-    printf("%.2f", (3.14*(a*a)*b)/3);
+    printf("V=%.2f\n", (3.14*(a*a)*b)/3);
+    L=sqrt(a*a+b*b);
+    printf("S=%.2f\n", 3.14*a*(a+L));
     return 0;
 }
