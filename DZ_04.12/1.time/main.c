c8aa8b6141698a31de11ffa1db4eccbd66e728b1
pirozhnikolga <pirozhenka@yandex.ru>
2014-12-02 23:41:49 +0400

    ttt

---
+++ DZ_04.12/1.time/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%02d:%02d:%02d\n", 16, 20, 14);
+    printf("%02d:%02d:%02d\n", 7, 13, 2);
+    printf("%02d:%02d:%02d\n", 11, 3, 0);
+    return 0;
+}
---
+++ DZ_04.12/3.weight/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a[]="Jorge", b[]="Maximilian", c[]="Annet", d[]="Alex";
+    double A=2.0, B=-2.115, C=-3.5, D=1.001;
+    printf("%10s | % .3f\n", a, A);
+    printf("%10s | % .3f\n", b, B);
+    printf("%10s | % .3f\n", c, C);
+    printf("%10s | % .3f\n", d, D);
+    return 0;
+}
