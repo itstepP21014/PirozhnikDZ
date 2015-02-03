f1f88ca576ba3a6ed9522a2cce044b6c6640bce0
pirozhnikolga <pirozhenka@yandex.ru>
2015-02-04 00:03:59 +0300

    не компилится

---
+++ DZ_S_dzC_014b_1/1.funs/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../mylib/include/funs_with_digit.h"
+
+int main()
+{
+    int N, M;
+    printf("Enter one number:\n");
+    scanf("%d", &N);
+    printf("%d\n", digitCount(N));
+    printf("%d\n", pickFirstFigure(N));
+    printf("%d\n", amputateFirstFigure(N));
+    printf("%d\n", shiftFirstFigure(N));
+    printf("Enter one else number:\n");
+    scanf("%d", &M);
+    printf("%d\n", glueFigures(int N, int M));
+    return 0;
+}
