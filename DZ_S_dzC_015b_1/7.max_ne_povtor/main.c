0fcaaee6520286ea144803e4fafb4bfcb494ae41
pirozhnikolga <pirozhenka@yandex.ru>
2015-02-08 22:36:44 +0300

    ...

---
+++ DZ_S_dzC_015b_1/7.max_ne_povtor/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int i=0;
+    int mas[i];
+    int max_single=mas[0];
+    for(i=0; i<=strlen(mas[i]); ++i){
+        if(mas[i]>max_single) max_single=mas[i];
+        esle
+    }
+    return 0;
+}
