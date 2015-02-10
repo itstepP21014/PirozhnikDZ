4e639654caf71008490bb8dc7645c0789eba9095
pirozhnikolga <pirozhenka@yandex.ru>
2015-02-11 00:46:05 +0300

    +

--- DZ_S_dzC_015b_1/1.max i min/main.c
+++ DZ_S_dzC_015b_1/1.max i min/main.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-int lastNegativeSearch(int a[], int sizeArray)
-{
-    for(int i=sizeArray; i>=1; --i){
-        if(a[i]<0){
-            return i;
-        }
-    }
-    return -1;
-}
-
-int firstPositiveSearch(int a[], int sizeArray)
-{
-    for(int i=1; i<=sizeArray; ++i){
-        if(a[i]>0){
-            return i;
-        }
-    }
-    return -1;
-}
-
+#include "max i min.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    int humanSize;
+    printf("Enter the size of array:\n");
+    scanf("%d", &humanSize);
+    int arr[humanSize];
+    input_rand(arr, humanSize);
+    output(arr, humanSize);
+    lastNegativeSearch(arr, humanSize);
+    firstPositiveSearch(arr, humanSize);
+    if(lastNegativeSearch(arr, humanSize)>0){
+        if(firstPositiveSearch(arr, humanSize)>0){
+            int zuzu=arr[lastNegativeSearch(arr, humanSize)];
+            arr[lastNegativeSearch(arr, humanSize)]=arr[firstPositiveSearch(arr, humanSize)];
+            arr[firstPositiveSearch(arr, humanSize)]=zuzu;
+            printf("\n\n");
+            output(arr, humanSize);
+        }else printf("There is no any positive element in the array.\n");
+    }else printf("There is no any negative element in the array.\n");
     return 0;
 }
+
