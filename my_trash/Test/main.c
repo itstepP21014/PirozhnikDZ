e55517cece9c77891f1f43fae38d71f163c27038
pirozhnikolga <pirozhenka@yandex.ru>
2015-05-12 15:17:44 +0000

    Update main.c

--- my_trash/Test/main.c
+++ my_trash/Test/main.c
@@ -1,72 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL.h>
-#include <SDL/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 #include <stdbool.h>
 
-#define BLACK_COLOR 0,0,0
-#define WHITE_COLOR 255,255,255
-#define RED_COLOR 255,0,0
-#define BLUE_COLOR 0,0,255
-
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-
-int main ( int argc, char** argv )
+int main()
 {
-
-    if(SDL_Init( SDL_INIT_EVERYTHING ) == -1) // инициализируем видео-подсистему
+    if(SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        printf("SDL_Init failed: %s\n", SDL_GetError() ); return false;
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        exit(1);
     }
 
-    if (TTF_Init() == -1)
+    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 800, 480, SDL_WINDOW_SHOWN);
+
+    if(win == NULL)
     {
-        printf("Unable to initialize SDL_ttf: %s \n", TTF_GetError());
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        exit(1);
     }
 
-    SDL_Surface* screen = NULL;
-    screen = SDL_CreateWindow("tex", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    if(screen == NULL)
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if(ren == NULL)
     {
-        printf("Unable to load screen: %s \n", SDL_GetError()); return false;
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_CreateRender Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
     }
 
-    TTF_Font* my_font = NULL;
-    my_font = TTF_OpenFont("Old Comedy.ttf", 24); // или (ARIAL.TTF", 24)
-    if(my_font == NULL)
-    { // если он не загрузился и остался NULL
-        printf("Unable to load font: %s \n", TTF_GetError()); return false;
-    }
+// ______текст_______________________________________________
+        TTF_Font* font = TTF_OpenFont("Old Comedy.ttf", 24);
+        if(font == NULL)
+        {
+            printf("Unable to load font: %s \n", TTF_GetError());
+            return false;
+        }
+        SDL_Color fore_color = {255, 0, 0, 255};
+        //SDL_Color back_color = { BLUE_COLOR };
 
-    SDL_Color fore_color = { RED_COLOR }; //255, 255 };
-    SDL_Color back_color = { BLUE_COLOR };
-
-    SDL_Surface* textSurface = NULL;
-    textSurface = TTF_RenderText_Shaded(my_font, "To be or not to be.", fore_color, back_color);
-    if(textSurface == NULL)
+//___картинка_____________________________________________________________
+    SDL_Surface *bmp = IMG_Load("../img/a.jpg");
+    if(bmp == NULL)
     {
-        printf("Unable to load textsurface: %s \n", SDL_GetError()); return false;
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_LoadBMP Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
     }
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+    SDL_FreeSurface(bmp);
+    if(tex == NULL)
+    {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
+    }
+//____картинка__________________________________________________________________
+    SDL_Surface *bmp2 = IMG_Load("../img/empty.jpg");
+    if(bmp2 == NULL)
+    {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_LoadBMP Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
+    }
+    SDL_Texture *tex2 = SDL_CreateTextureFromSurface(ren, bmp2);
+    SDL_FreeSurface(bmp);
+    if(tex2 == NULL)
+    {
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
+    }
+//***********************************************************************************
+    SDL_RenderClear(ren);
+//************************************************************************************
 
-    // Pass zero for width and height to draw the whole surface
-    SDL_Rect textLocation = { 100, 200, 0, 0 };
+    //___для картинок_______________________________________________________________
+    SDL_Rect Card = {0,0,150,90};
 
-    SDL_BlitSurface(textSurface, NULL, screen, &textLocation); // скопируем тектовую сурфейс на экранную screen
+    SDL_Rect pos = {50,50,300,90};
+    SDL_RenderCopy(ren, tex, &Card, &pos);
 
-    //SDL_Flip(screen); // отобразим сарфейс screen
-    SDL_RenderPresent(textSurface);
+    SDL_Rect pos2 = {350,50,300,90};
 
-    SDL_Delay(3000); // подождем
+    //_____для текста________________________________________________________
 
-    SDL_FreeSurface(textSurface); // освободим поверхность для текста
 
-    TTF_CloseFont(my_font); // закроем используемый шрифт
+//************************************************************************************
+    SDL_RenderCopy(ren, tex2, &Card, &pos2);
+//************************************************************************************
+    SDL_RenderPresent(ren);
+    SDL_Delay(2000);
 
-    TTF_Quit(); // закроем библиотеку шрифтов
-
-    SDL_Quit(); // закроем SDL
+    SDL_DestroyTexture(tex);
+    SDL_DestroyTexture(tex2);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     return 0;
-
 }
