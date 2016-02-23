deb28274b0ea48f3c759985b8a7a9c0849c272d7
pirozhnikolga <pirozhenka@yandex.ru>
2016-02-23 21:09:28 +0300

    взаимодействие потоков

---
+++ Системное программирование/Inkrement/Inkrement/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inkrement
+{
+    class Program
+    {
+        class Worker
+        {
+            public static int N {get; set;}
+            public static int M { get; set; }
+            static Object Monitor = new Object(); // говорит занята ли критическая секция или нет
+            static void func()
+            {
+                Random r = new Random();
+                for (int i = 10; i < 20; ++i)
+                {
+                    // критическая секция, в нее моет зайти только один поток
+                    lock (Monitor)
+                    {
+                        ++N;
+                        ++M;
+                    }
+                }
+            }
+
+            public static void start()
+            {
+                Thread thread = new Thread(func);
+                thread.Start();
+
+            }
+
+            //public Worker()
+            //{
+            //    Thread thread = new Thread(func);
+            //    thread.Start();
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Worker.start();
+            Worker.start();
+            Thread.Sleep(1000);
+            Console.WriteLine("Main {0}", Worker.N);
+        }
+
+        static void Main22(string[] args)
+        {
+            Worker a = new Worker();
+            Worker b = new Worker();
+            Console.WriteLine(Worker.N);
+        }
+
+        static void Main11(string[] args)
+        {
+            Thread myThread1 = new Thread(myFunction);
+            Thread myThread2 = new Thread(myFunction);
+            myThread1.IsBackground = true; // делаем один поток фотовым, даже если он не закончится, приложение сможет закрыться
+            Console.WriteLine(myThread1.IsBackground);
+            myThread1.Start(1);
+            myThread2.Start(2);
+
+            for (int i = 10; i <= 20; ++i)
+            {
+                Console.WriteLine(myThread1.IsAlive);
+            }
+        }
+
+        static void myFunction(Object obj) // функции, передающиеся потоку, должны принимать только object
+        {
+            int n = (int)obj; // преобразуем входное значение уже только внутри функции
+            for(int i = 0; i <= 20; ++i)
+            {
+                Console.WriteLine("{0}: {1}", n, i);
+                Thread.Sleep(100); // операционна система не дает потоку управление на 100 милисекунд
+            }
+        }
+    }
+}
---
+++ Системное программирование/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEmployees_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "All files (*.*)|*.*|PNG Photos (*.png)|*.png";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+
+            if (ofd.ShowDialog() == null)
+            {
+                try
+                {
+                    if ((myStream = ofd.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
