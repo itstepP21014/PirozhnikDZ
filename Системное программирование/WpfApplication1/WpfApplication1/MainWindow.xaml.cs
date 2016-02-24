391eb2c37018fe130e3fb40527472bc4c6c7af61
pirozhnikolga <pirozhenka@yandex.ru>
2016-02-24 21:23:24 +0300

    progressbar done

--- Системное программирование/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ Системное программирование/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApplication1
 {
@@ -25,34 +29,81 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
+        static Thread threadCopy, threadProgressBar;
+        static string start, destination;
+        static object monitor = new object();
+        static double value;
 
-        private void btnEmployees_Click(object sender, RoutedEventArgs e)
+        private string GetFilePath()
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.InitialDirectory = "c:\\";
-            ofd.Filter = "All files (*.*)|*.*|PNG Photos (*.png)|*.png";
-            ofd.FilterIndex = 2;
-            ofd.RestoreDirectory = true;
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = @"C:\Users\admin\Desktop";
+            if (file.ShowDialog() == null)
+                return "";
 
-            if (ofd.ShowDialog() == null)
+            return file.FileName;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            start = this.GetFilePath();
+            btnStart.Content = start;
+        }
+
+        private void btnDestination_Click(object sender, RoutedEventArgs e)
+        {
+            destination = this.GetFilePath();
+            btnDestination.Content = destination;         
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            threadCopy = new Thread(CopyFile);
+            threadProgressBar = new Thread(RanProgressBar);
+            threadCopy.Start();
+            threadProgressBar.Start();
+        }
+
+        void RanProgressBar()
+        {
+            while(threadCopy.IsAlive)
             {
-                try
-                {
-                    if ((myStream = ofd.OpenFile()) != null)
-                    {
-                        using (myStream)
-                        {
-                            // Insert code to read the stream here.
+                Dispatcher.Invoke(new Action(() => pb.Value = value));
+            }
+            Dispatcher.Invoke(new Action(() => pb.Value = 0));
+        }
 
-                        }
+        static void CopyFile()
+        {
+            lock (monitor)
+            {
+                using (var outputFile = File.OpenRead(start))
+                {
+                    using (var inputFile = File.OpenWrite(destination))
+                    {
+                        int CopyBufferSize = (int)outputFile.Length / 100;
+                        var buffer = new byte[CopyBufferSize];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead = outputFile.Read(buffer, 0, CopyBufferSize);
+                            if (bytesRead != 0)
+                            {
+                                inputFile.Write(buffer, 0, bytesRead);
+                            }
+                            Thread.Sleep(10);
+                            value = (inputFile.Length * 1.0) * 100 / (outputFile.Length * 1.0);
+                           
+                        } while (bytesRead != 0);
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
-                }
+                MessageBox.Show("Copying has completed!");
             }
         }
+
+
+
+
 
 
     }
