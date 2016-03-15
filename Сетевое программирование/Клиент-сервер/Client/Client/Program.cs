6d6cd62c95e662a7dca7fab270f3ef8a3c9fada0
pirozhnikolga <pirozhenka@yandex.ru>
2016-03-15 21:17:56 +0300

    сетевое прграммирование, 1 заняте

---
+++ Сетевое программирование/Клиент-сервер/Client/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вы:");
+            string strSend = Console.ReadLine();
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1024);
+
+            s.Bind(ep);
+            s.Listen(1);
+
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    ns.Send(System.Text.Encoding.ASCII.GetBytes(strSend));
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Сетевое программирование/Клиент-сервер/Server/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(ip, 1024);
+
+            s.Bind(ep);
+            s.Listen(1);
+
+            try
+            {
+                while (true)
+                {
+                    Socket ns = s.Accept();
+                    byte[] buffer = new byte[1024];
+                    int l;
+                    do
+                    {
+                        l = s.Receive(buffer);
+                        Console.WriteLine("Клиент: {0}", System.Text.Encoding.ASCII.GetString(buffer, 0, 1));
+                    } while (true);
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
