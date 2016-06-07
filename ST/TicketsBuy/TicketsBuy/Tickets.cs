06b5f6cbcafcb2679e5188c412ee33bf7e327172
Dmitry Mokeychyk <Dmitry.Mokeychyk@gmail.com>
2016-06-07 19:20:53 +0300

    tickets buy

---
+++ ST/TicketsBuy/TicketsBuy/Tickets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsBuy
+{
+    public class Tickets
+    {
+        public int Id { get; set; }
+        public string Dispatch_station { get; set; }
+        public string Arrival_station { get; set; }
+        public string Time { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ ST/TicketsBuy/TicketsBuy/TicketsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsBuy
+{
+    public class TicketsContext: DbContext
+    {
+        public TicketsContext()
+            : base("DefaultConnection")
+        {
+ 
+        }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
