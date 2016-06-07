81e36723516059e6921dfbda415258fc96c8b095
pirozhnikolga <pirozhenka@yandex.ru>
2016-06-07 18:46:15 +0300

    unit test

---
+++ ST/ApplicationTest/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            StringBuilder strOne = ApplicationTest.Program.returnStringBilder("123");
+            StringBuilder strTwo = ApplicationTest.Program.returnStringBilder("abc");
+            StringBuilder strExpected = new StringBuilder("123abc");
+            StringBuilder strAppend = strOne.Append(strTwo);
+            Assert.AreEqual(strExpected.ToString(), strAppend.ToString());
+
+        }
+    }
+}
