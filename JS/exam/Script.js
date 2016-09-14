db0070d114250210a19ec8e62115fa39d9a1848a
pirozhnikolga <pirozhenka@yandex.ru>
2016-09-14 21:28:53 +0300

    igra

--- JS/exam/Script.js
+++ JS/exam/Script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 
-    $(".square").hover(function () {
-        $(this).fadeOut("fast").fadeIn("fast");
+    $(".flip-container").click(function () {
+
+        //$(this.childNodes[1]).animate({ transform: 'rotateY(180deg)' }, 1000);
+        this.childNodes[1].style.transform = "rotateY(180deg)";
     });
 
-    $(".card-grid").flip('toggle');
 })
 
 
