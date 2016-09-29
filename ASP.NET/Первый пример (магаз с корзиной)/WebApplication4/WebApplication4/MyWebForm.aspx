6fac8744804084227574b77e6ec807e767fa0b73
pirozhnikolga <pirozhenka@yandex.ru>
2016-09-29 21:23:23 +0300

    fgth

--- ASP.NET/Первый пример (магаз с корзиной)/WebApplication4/WebApplication4/MyWebForm.aspx
+++ ASP.NET/Первый пример (магаз с корзиной)/WebApplication4/WebApplication4/MyWebForm.aspx
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
+<head runat="server" dir="auto">
     <title></title>
 </head>
 <body>
@@ -18,12 +18,12 @@
         <asp:CheckBoxList ID="CheckBoxList1" runat="server"></asp:CheckBoxList>--%>
         <div style="display: inline-block; margin-right: 20px">
             <asp:Label ID="LabelProducts" runat="server" Text="Label">Список продуктов:</asp:Label><br />   
-            <asp:ListBox ID="ListBoxProducts" runat="server"  style="width: 200px"></asp:ListBox><br />
+            <asp:ListBox ID="ListBoxProducts" runat="server"  style="width: 200px" SelectionMode="Multiple" SelectedIndexChanged="ListBoxProducts_SelectedIndexChanged"></asp:ListBox><br />
             <asp:Button ID="ButtonToBusket" runat="server" Text="Отправить в корзину"  OnClick="ButtonToBusket_Click"/>
         </div>
         <div style="display: inline-block">
             <asp:Label ID="LabelBusket" runat="server" Text="Label">Корзина:</asp:Label><br />
-            <asp:ListBox ID="ListBoxBusket" runat="server" style="width: 200px"></asp:ListBox><br />
+            <asp:ListBox ID="ListBoxBusket" runat="server" style="width: 200px" SelectionMode="Multiple" SelectedIndexChanged="ListBoxBusket_SelectedIndexChanged"></asp:ListBox><br />
             <asp:Button ID="ButtonFromBusket" runat="server" Text="Убрать из корзины" OnClick="ButtonFromBusket_Click"/>
         </div>
      </div>
--- ASP.NET/Первый пример (магаз с корзиной)/WebApplication4/WebApplication4/MyWebForm.aspx.cs
+++ ASP.NET/Первый пример (магаз с корзиной)/WebApplication4/WebApplication4/MyWebForm.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,34 +10,38 @@ namespace WebApplication4
 {
     public partial class MyWebForm : System.Web.UI.Page
     {
+
+        List<Product> collection = new List<Product>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 var products = GetProducts();
-                products.ForEach(p =>
-                    {
-                        ListBoxProducts.Items.Add(p.Name);
-                    });
+                products.ForEach(p => { ListBoxProducts.Items.Add(p.Name); } );
             }
 
         }
-        // protected void Button1_Click(object sender, EventArgs e)
-        //{
-        //    this.Response.Output.WriteLine("Hello!");
 
-        //}
 
         protected void ButtonToBusket_Click(object sender, EventArgs e)
         {
-            
-
+            foreach (var i in collection)
+            {
+                ListBoxBusket.Items.Add(i.ToString());
+                ListBoxProducts.Items.Remove(i.ToString());
+            }
+            collection.Clear();
         }
 
         protected void ButtonFromBusket_Click(object sender, EventArgs e)
         {
-            
-
+            foreach (var i in collection)
+            {
+                ListBoxProducts.Items.Add(i.ToString());
+                ListBoxProducts.Items.Remove(i.ToString());
+            }
+            collection.Clear();
         }
 
         protected class Product
@@ -47,15 +52,47 @@ namespace WebApplication4
         protected List<Product> GetProducts()
         {
             var list = new List<Product>();
-            list.Add(new Product() { Name="понедельник"});
-            list.Add(new Product() { Name = "вторник" });
-            list.Add(new Product() { Name = "среда" });
-            list.Add(new Product() { Name = "четверг" });
-            list.Add(new Product() { Name = "пятница" });
-            list.Add(new Product() { Name = "суббота" });
-            list.Add(new Product() { Name = "воскресенье" });
+            list.Add(new Product() { Name="бананы"});
+            list.Add(new Product() { Name = "яблоки" });
+            list.Add(new Product() { Name = "груша" });
+            list.Add(new Product() { Name = "капуста" });
+            list.Add(new Product() { Name = "картофель" });
+            list.Add(new Product() { Name = "огурцы" });
+            list.Add(new Product() { Name = "помидоры" });
    
             return list;
+        }
+
+        protected void ListBoxProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            //var builder = new StringBuilder();
+
+            foreach (ListItem i in ListBoxProducts.Items)
+            {
+                if (i.Selected)
+                {
+                    //builder.AppendLine(i.Value);
+                    collection.Add(new Product() { Name = i.Value });
+                }
+            }
+            
+            //Repeater1.DataSource = collection;
+            //Repeater1.DataBind();
+            //this.Response.Output.WriteLine(builder.ToString());
+
+        }
+
+        protected void ListBoxBusket_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            foreach (ListItem i in ListBoxBusket.Items)
+            {
+                if (i.Selected)
+                {
+                    collection.Add(new Product() { Name = i.Value });
+                }
+            }
         }
     }
 }
